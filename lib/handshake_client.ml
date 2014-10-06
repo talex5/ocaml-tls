7d298b4a8645452f8eef6b128d7ff69ee16e84cf
Hannes Mehnert <hannes@mehnert.org>
2014-10-06 23:55:10 +0200

    move peer_rsa_key, verify_digitally_signed, and validate_chain to handshake_common

--- lib/handshake_client.ml
+++ lib/handshake_client.ml
@@ -106,85 +106,22 @@ let answer_server_hello_renegotiate state session ch (sh : server_hello) raw log
   in
   ({ state with machina = Client machina }, [])
 
-
-let validate_chain config session certificates =
-  let open Certificate in
-
-  let parse css =
-    match parse_stack css with
-    | None       -> fail Packet.BAD_CERTIFICATE
-    | Some stack -> return stack
-
-  and authenticate authenticator server_name ((server_cert, _) as stack) =
-    match
-      X509.Authenticator.authenticate ?host:server_name authenticator stack
-    with
-    | `Fail SelfSigned         -> fail Packet.UNKNOWN_CA
-    | `Fail NoTrustAnchor      -> fail Packet.UNKNOWN_CA
-    | `Fail CertificateExpired -> fail Packet.CERTIFICATE_EXPIRED
-    | `Fail _                  -> fail Packet.BAD_CERTIFICATE
-    | `Ok anchor               -> return anchor
-
-  and validate_keytype cert ktype =
-    cert_type cert = ktype
-
-  and validate_usage cert usage =
-    match cert_usage cert with
-    | None        -> true
-    | Some usages -> List.mem usage usages
-
-  and validate_ext_usage cert ext_use =
-    match cert_extended_usage cert with
-    | None            -> true
-    | Some ext_usages -> List.mem ext_use ext_usages || List.mem `Any ext_usages
-
-  and key_size min cs =
-    let check c =
-      let open Asn_grammars in
-      ( match Certificate.(asn_of_cert c).tbs_cert.pk_info with
-        | PK.RSA key when Rsa.pub_bits key >= min -> true
-        | _                                       -> false )
-    in
-    guard (List.for_all check cs) Packet.INSUFFICIENT_SECURITY
-
-  and host = match config.peer_name with
+let answer_certificate_RSA state session cs raw log =
+  let cfg = state.config in
+  let peer_name = match cfg.peer_name with
     | None   -> None
     | Some x -> Some (`Wildcard x)
   in
-
-  (* RFC5246: must be x509v3, take signaturealgorithms into account! *)
-  (* RFC2246/4346: is generally x509v3, signing algorithm for certificate _must_ be same as algorithm for certificate key *)
-
-  match config.authenticator with
-  | None -> parse certificates >|= fun (s, xs) ->
-            (s, { session with peer_certificate = s :: xs })
-  | Some authenticator ->
-      parse certificates >>= fun (s, xs) ->
-      key_size Config.min_rsa_key_size (s :: xs) >>= fun () ->
-      authenticate authenticator host (s, xs) >>= fun anchor ->
-      let keytype, usage =
-        Ciphersuite.(o required_keytype_and_usage ciphersuite_kex session.ciphersuite)
-      in
-      guard (validate_keytype s keytype &&
-             validate_usage s usage &&
-             validate_ext_usage s `Server_auth)
-            Packet.BAD_CERTIFICATE >|= fun () ->
-      (s, { session with peer_certificate = s :: xs ; trust_anchor = Some anchor })
-
-let peer_rsa_key cert =
-  let open Asn_grammars in
-  match Certificate.(asn_of_cert cert).tbs_cert.pk_info with
-  | PK.RSA key -> return key
-  | _          -> fail_handshake
-
-let answer_certificate_RSA state session cs raw log =
-  validate_chain state.config session cs >>= fun (cert, session) ->
+  let keytype, usage =
+    Ciphersuite.(o required_keytype_and_usage ciphersuite_kex session.ciphersuite)
+  in
+  validate_chain cfg.authenticator cs session peer_name keytype usage >>= fun session ->
   ( match session.client_version with
     | Supported v -> return v
     | _           -> fail_handshake ) >>= fun v ->
   let ver = Writer.assemble_protocol_version v in
   let premaster = ver <+> Rng.generate 46 in
-  peer_rsa_key cert >|= fun pubkey ->
+  peer_rsa_key (List.hd session.peer_certificate) >|= fun pubkey ->
   let kex = Rsa.PKCS1.encrypt pubkey premaster
   in
 
@@ -195,7 +132,15 @@ let answer_certificate_RSA state session cs raw log =
   ({ state with machina = Client machina }, [])
 
 let answer_certificate_DHE_RSA state session cs raw log =
-  validate_chain state.config session cs >|= fun (_, session) ->
+  let cfg = state.config in
+  let peer_name = match cfg.peer_name with
+    | None   -> None
+    | Some x -> Some (`Wildcard x)
+  in
+  let keytype, usage =
+    Ciphersuite.(o required_keytype_and_usage ciphersuite_kex session.ciphersuite)
+  in
+  validate_chain cfg.authenticator cs session peer_name keytype usage >|= fun session ->
   let machina = AwaitServerKeyExchange_DHE_RSA (session, log @ [raw]) in
   ({ state with machina = Client machina }, [])
 
@@ -205,47 +150,12 @@ let answer_server_key_exchange_DHE_RSA state session kex raw log =
     match parse_dh_parameters kex with
     | Or_error.Ok data -> return data
     | Or_error.Error _ -> fail_handshake
-
-  and signature_verifier version data =
-    match version with
-    | TLS_1_0 | TLS_1_1 ->
-        ( match parse_digitally_signed data with
-          | Or_error.Ok signature ->
-             let compare_hashes should data =
-               let computed_sig = Hash.(MD5.digest data <+> SHA1.digest data) in
-               assure (Cs.equal should computed_sig)
-             in
-             return (signature, compare_hashes)
-          | Or_error.Error _      -> fail_handshake )
-    | TLS_1_2 ->
-       ( match parse_digitally_signed_1_2 data with
-         | Or_error.Ok (hash_algo, Packet.RSA, signature) ->
-            let compare_hashes should data =
-              match Asn_grammars.pkcs1_digest_info_of_cstruct should with
-              | Some (hash_algo', target) when hash_algo = hash_algo' ->
-                 ( match Crypto.digest_eq hash_algo ~target data with
-                   | true  -> return ()
-                   | false -> fail_handshake )
-              | _ -> fail_handshake
-            in
-            return (signature, compare_hashes)
-         | _ -> fail_handshake )
-
-  and signature pubkey raw_signature =
-    match Rsa.PKCS1.verify pubkey raw_signature with
-    | Some signature -> return signature
-    | None -> fail_handshake
-
   in
 
   dh_params kex >>= fun (dh_params, raw_dh_params, leftover) ->
-  signature_verifier state.protocol_version leftover >>= fun (raw_signature, verifier) ->
-  (match session.peer_certificate with
-   | cert :: _ -> peer_rsa_key cert
-   | []        -> fail_handshake ) >>= fun pubkey ->
-  signature pubkey raw_signature >>= fun signature ->
   let sigdata = session.client_random <+> session.server_random <+> raw_dh_params in
-  verifier signature sigdata >>= fun () ->
+  verify_digitally_signed state.protocol_version leftover sigdata session.peer_certificate
+  >>= fun () ->
   let group, shared = Crypto.dh_params_unpack dh_params in
   guard (Dh.apparent_bit_size group >= Config.min_dh_size) Packet.INSUFFICIENT_SECURITY
   >>= fun () ->
--- lib/handshake_common.ml
+++ lib/handshake_common.ml
@@ -164,3 +164,109 @@ let signature version data sig_algs hashes private_key =
      let hash = Hash.digest hash_algo data in
      let cs = Asn_grammars.pkcs1_digest_info_to_cstruct (hash_algo, hash) in
      sign cs >|= Writer.assemble_digitally_signed_1_2 hash_algo Packet.RSA
+
+let peer_rsa_key cert =
+  let open Asn_grammars in
+  match Certificate.(asn_of_cert cert).tbs_cert.pk_info with
+  | PK.RSA key -> return key
+  | _          -> fail_handshake
+
+let verify_digitally_signed version data signature_data certificate =
+  let signature_verifier version data =
+    let open Reader in
+    match version with
+    | TLS_1_0 | TLS_1_1 ->
+        ( match parse_digitally_signed data with
+          | Or_error.Ok signature ->
+             let compare_hashes should data =
+               let computed_sig = Hash.(MD5.digest data <+> SHA1.digest data) in
+               assure (Cs.equal should computed_sig)
+             in
+             return (signature, compare_hashes)
+          | Or_error.Error _      -> fail_handshake )
+    | TLS_1_2 ->
+       ( match parse_digitally_signed_1_2 data with
+         | Or_error.Ok (hash_algo, Packet.RSA, signature) ->
+            let compare_hashes should data =
+              match Asn_grammars.pkcs1_digest_info_of_cstruct should with
+              | Some (hash_algo', target) when hash_algo = hash_algo' ->
+                 ( match Crypto.digest_eq hash_algo ~target data with
+                   | true  -> return ()
+                   | false -> fail_handshake )
+              | _ -> fail_handshake
+            in
+            return (signature, compare_hashes)
+         | _ -> fail_handshake )
+
+  and signature pubkey raw_signature =
+    match Rsa.PKCS1.verify pubkey raw_signature with
+    | Some signature -> return signature
+    | None -> fail_handshake
+
+  in
+
+  signature_verifier version data >>= fun (raw_signature, verifier) ->
+  (match certificate with
+   | cert :: _ -> peer_rsa_key cert
+   | []        -> fail_handshake ) >>= fun pubkey ->
+  signature pubkey raw_signature >>= fun signature ->
+  verifier signature signature_data
+
+(* TODO: extended_key_usage *)
+let validate_chain authenticator certificates session hostname keytype usage =
+  let open Certificate in
+
+  let parse css =
+    match parse_stack css with
+    | None       -> fail Packet.BAD_CERTIFICATE
+    | Some stack -> return stack
+
+  and authenticate authenticator server_name ((server_cert, _) as stack) =
+    match
+      X509.Authenticator.authenticate ?host:server_name authenticator stack
+    with
+    | `Fail SelfSigned         -> fail Packet.UNKNOWN_CA
+    | `Fail NoTrustAnchor      -> fail Packet.UNKNOWN_CA
+    | `Fail CertificateExpired -> fail Packet.CERTIFICATE_EXPIRED
+    | `Fail _                  -> fail Packet.BAD_CERTIFICATE
+    | `Ok anchor               -> return anchor
+
+  and validate_keytype cert ktype =
+    cert_type cert = ktype
+
+  and validate_usage cert usage =
+    match cert_usage cert with
+    | None        -> true
+    | Some usages -> List.mem usage usages
+
+  and validate_ext_usage cert ext_use =
+    match cert_extended_usage cert with
+    | None            -> true
+    | Some ext_usages -> List.mem ext_use ext_usages || List.mem `Any ext_usages
+
+  and key_size min cs =
+    let check c =
+      let open Asn_grammars in
+      ( match Certificate.(asn_of_cert c).tbs_cert.pk_info with
+        | PK.RSA key when Rsa.pub_bits key >= min -> true
+        | _                                       -> false )
+    in
+    guard (List.for_all check cs) Packet.INSUFFICIENT_SECURITY
+
+  in
+
+  (* RFC5246: must be x509v3, take signaturealgorithms into account! *)
+  (* RFC2246/4346: is generally x509v3, signing algorithm for certificate _must_ be same as algorithm for certificate key *)
+
+  match authenticator with
+  | None -> parse certificates >|= fun (s, xs) ->
+            { session with peer_certificate = s :: xs }
+  | Some authenticator ->
+      parse certificates >>= fun (s, xs) ->
+      key_size Config.min_rsa_key_size (s :: xs) >>= fun () ->
+      authenticate authenticator hostname (s, xs) >>= fun anchor ->
+      guard (validate_keytype s keytype &&
+             validate_usage s usage &&
+             validate_ext_usage s `Server_auth)
+            Packet.BAD_CERTIFICATE >|= fun () ->
+      { session with peer_certificate = s :: xs ; trust_anchor = Some anchor }
